bb87a808681a1b19f1916d2427fa93207328dd21
Ben Hoff <hoff.benjamin.k@gmail.com>
2016-01-21 11:17:35 -0600

    added in main file

---
+++ src/__main__.py
@@ -0,0 +1,15 @@
+import argparse
+
+def main(args=None):
+    parser = argparse.ArgumentParser(prog='Vex',
+                                     description='Speech to text using PyAudio and choice of speech analyzer')
+
+    parser.add_argument(['--provider', '-p'],
+                        action='store')
+    
+    # TODO: add in option to specify a file like mp3
+    # parser.add_argument(['-file', '-f'])
+
+
+if __name__ == '__main__':
+    main()
