266fdade3ec51946f414325e8e382576dd36a5b1
Ben Hoff <hoff.benjamin.k@gmail.com>
2016-01-21 10:32:05 -0600

    modified gitignore for dist folder and egg info. added setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import find_packages, setup
+
+
+# directory = os.path.abspath(os.path.dirname(__file__))
+"""
+with open(os.path.join(directory, 'README.rst')) as f:
+    long_description = f.read()
+"""
+
+setup(
+    name="microphone",
+    version='0.0.1',
+    description='Text to Speech for python',
+    # long_description=long_description,
+    url='https://github.com/benhoff/microphone',
+    license='GPL3',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Multimedia :: Sound/Audio :: Analysis',
+        'Topic :: Multimedia :: Sound/Audio :: Capture/Recording',
+        'Topic :: Utilities',
+        'Operating System :: OS Independent'],
+    keywords='sound capture to text',
+    author='Ben Hoff',
+    author_email='beohoff@gmail.com',
+    packages= find_packages(), # exclude=['docs', 'tests']
+)
