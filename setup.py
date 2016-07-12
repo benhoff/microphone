a20ac87f251cc767d5dc31c7b608d8298f85d127
Ben Hoff <hoff.benjamin.k@gmail.com>
2016-07-11 20:58:49 -0600

    bump version after changes

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(directory, 'README.rst')) as f:
 
 setup(
     name="microphone",
-    version='0.2.2',
+    version='0.2.3',
     description='Sending microphone data using ZeroMQ',
     # long_description=long_description,
     url='https://github.com/benhoff/microphone',
